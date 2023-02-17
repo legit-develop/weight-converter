bed277e643e0f11b09bf7501d24c61012166e25f
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-17 16:30:00 +0200

    complete javascript functionality

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const inputEl = document.getElementById("input");
 const errorEl = document.getElementById("error");
+const resultEl = document.getElementById("result");
+
+let errorTime;
+let resultTime;
 
 function updateResults() {
   if (inputEl.value <= 0 || isNaN(inputEl.value)) {
     errorEl.innerText = `${inputEl.value} is invalid input`;
-    setTimeout(() => {
+    clearTimeout(errorTime);
+    errorTime = setTimeout(() => {
       errorEl.innerText = "";
+      inputEl.value = "";
     }, 2000);
+  } else {
+    resultEl.innerText = (+inputEl.value / 2.2).toFixed(1);
+    clearTimeout(resultTime);
+    resultTime = setTimeout(() => {
+      resultEl.innerText = "";
+      inputEl.value = "";
+    }, 10000);
   }
 }
 
